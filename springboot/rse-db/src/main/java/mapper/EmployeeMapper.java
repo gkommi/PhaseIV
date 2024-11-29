c0f6ae0a617a96c065c36c2a98228dc16825072d
arihantbirani <arihantbirani31@gmail.com>
2024-11-29 14:46:00 -0500

    Employee, Location, Owner Mappers

--- springboot/rse-db/src/main/java/mapper/EmployeeMapper.java
+++ springboot/rse-db/src/main/java/mapper/EmployeeMapper.java
@@ -1,3 +1,7 @@
-public class EmployeeMapper {
-    
+public interface EmployeeMapper {
+    List<Employee> displayEmployeeView();
+    List<Employee> displayEmployee();
+    Integer addEmployee(Employee employee);
+    Integer hireEmployee(String username, String id);
+    Integer fireEmployee(String username, String id);
 }
